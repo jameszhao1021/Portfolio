4aac8a11da2e8af6faff71a8c73dec43adb54f89
james <351917842@qq.com>
2024-05-01 21:24:08 +0800

    layout modified

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,14 +4,14 @@ import NetMall from '../NetMallPage/NetMall';
 import TravelBlog from '../TravelBlogPage/TravelBlog';
 import Roulette from '../RoulettePage/Roulette';
 import PlantDiary from '../PlantDiaryPage/PlantDiary'
-import { Routes, Route, useLocation, Router, Navigate } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import React from 'react';
 import { useState, useRef, useEffect } from 'react';
 import Header from "../../components/Header/Header";
 import Footer from '../../components/Footer/Footer';
 import ProjectHeader from '../../components/ProjectHeader/ProjectHeader';
-import { motion, useInView } from 'framer-motion'
-import Layout from '../../hocs/Layout'
+import { useInView } from 'framer-motion'
+
 
 function App() {
   const aboutTargetRef = useRef(null);
@@ -34,32 +34,33 @@ function App() {
   // }
 
   return (
-    // <div className='appContainer'>
-    //   {isHomeRoute ? (
-    //     <Header aboutTargetRef={aboutTargetRef} projectTargetRef={projectTargetRef} contactTargetRef={contactTargetRef} />
-    //   ) : (
-    //     <ProjectHeader />
-    //   )}
-    //   <Routes>
-    //     <Route path='/' element={<Home aboutTargetRef={aboutTargetRef} projectTargetRef={projectTargetRef} contactTargetRef={contactTargetRef} isGreeting={isGreeting} greetingRef={greetingRef}/>} />
-    //     <Route path='/travelblog' element={<TravelBlog isGreeting={isGreeting} greetingRef={greetingRef}/>} />
-    //     <Route path='/roulette' element={<Roulette isGreeting={isGreeting} greetingRef={greetingRef}/>} />
-    //     <Route path='/plantdiary' element={<PlantDiary isGreeting={isGreeting} greetingRef={greetingRef}/>} />
-    //   </Routes>
-    //   <Footer />
-    // </div>
+    <>
+      {isHomeRoute ? (
+        <Header aboutTargetRef={aboutTargetRef} projectTargetRef={projectTargetRef} contactTargetRef={contactTargetRef} />
+      ) : (
+        <ProjectHeader />
+      )}
+      <Routes>
+        <Route path='/' element={<Home aboutTargetRef={aboutTargetRef} projectTargetRef={projectTargetRef} contactTargetRef={contactTargetRef} isGreeting={isGreeting} greetingRef={greetingRef}/>} />
+        <Route path='/netmall' element={<NetMall isGreeting={isGreeting} greetingRef={greetingRef} />} />
+        <Route path='/travelblog' element={<TravelBlog isGreeting={isGreeting} greetingRef={greetingRef}/>} />
+        <Route path='/roulette' element={<Roulette isGreeting={isGreeting} greetingRef={greetingRef}/>} />
+        <Route path='/plantdiary' element={<PlantDiary isGreeting={isGreeting} greetingRef={greetingRef}/>} />
+      </Routes>
+      <Footer />
+    </>
 
     // <div className='appContainer'>
    
-        <Layout >
-          <Routes>
-            <Route path='/' element={<Home aboutTargetRef={aboutTargetRef} projectTargetRef={projectTargetRef} contactTargetRef={contactTargetRef} isGreeting={isGreeting} greetingRef={greetingRef} />} />
-            <Route path='/netmall' element={<NetMall isGreeting={isGreeting} greetingRef={greetingRef} />} />
-            <Route path='/travelblog' element={<TravelBlog isGreeting={isGreeting} greetingRef={greetingRef} />} />
-            <Route path='/roulette' element={<Roulette isGreeting={isGreeting} greetingRef={greetingRef} />} />
-            <Route path='/plantdiary' element={<PlantDiary isGreeting={isGreeting} greetingRef={greetingRef} />} />
-          </Routes>
-        </Layout>
+        // <Layout >
+        //   <Routes>
+        //     <Route path='/' element={<Home aboutTargetRef={aboutTargetRef} projectTargetRef={projectTargetRef} contactTargetRef={contactTargetRef} isGreeting={isGreeting} greetingRef={greetingRef} />} />
+        //     <Route path='/netmall' element={<NetMall isGreeting={isGreeting} greetingRef={greetingRef} />} />
+        //     <Route path='/travelblog' element={<TravelBlog isGreeting={isGreeting} greetingRef={greetingRef} />} />
+        //     <Route path='/roulette' element={<Roulette isGreeting={isGreeting} greetingRef={greetingRef} />} />
+        //     <Route path='/plantdiary' element={<PlantDiary isGreeting={isGreeting} greetingRef={greetingRef} />} />
+        //   </Routes>
+        // </Layout>
    
     // </div>
   );
